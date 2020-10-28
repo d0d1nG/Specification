9487078fc00eaebfd5d719dc982d50ada31fdb92
Optimus <Optimus@DESKTOP-8KKQ6H9>
2020-10-28 13:50:45 +0100

    api class

--- Specification/src/utils/API.java
+++ Specification/src/utils/API.java
@@ -1,6 +1,32 @@
 package utils;
 
+import java.io.File;
+
 public abstract class API 
 {
-
+	public API() {
+	}
+	
+	abstract public void importFile(String dir, Class<?> classOf);
+	
+	abstract public void exportObject(String dir, Object obj);
+	
+	abstract public void openFile(File file);
+	
+	/**
+	 * Otvara sve fajlove iz zeljenog direktorijuma
+	 * U direktorijumu se nalaze fajlovi jednog formata
+	 * @param path
+	 */
+	
+	public void openDir(String path)
+	{
+		File directory = new File(path);
+		File[] contents = directory.listFiles();
+		
+		for(File f : contents) 
+		{
+			openFile(f);
+		}
+	}
 }
