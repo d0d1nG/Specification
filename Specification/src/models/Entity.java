62f796a91259a1dca326466c06d149aca8a6b67c
DESKTOP-L6O7F6S\bosko <boskovicnikola99@gmail.com>
2020-10-31 18:20:22 +0100

    Komit

---
+++ Specification/src/models/Entity.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Entity {
+	private String id;
+	private String name;
+	
+	private Map<String, Object> properties = new HashMap<String, Object>();
+	private Map<String, Entity> entities = new HashMap<String, Entity>();
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+	public void setProperties(Map<String, Object> properties) {
+		this.properties = properties;
+	}
+	public Map<String, Entity> getEntities() {
+		return entities;
+	}
+	public void setEntities(Map<String, Entity> entities) {
+		this.entities = entities;
+	}
+	@Override
+	public String toString() {
+		return "Entity [id=" + id + ", name=" + name + ", properties=" + properties.toString() + ", entities=" + entities.toString() + "]";
+	}
+	
+	
+}
